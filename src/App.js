e87641725ba95abd01863bb1e5a64ca8fa88aa78
Prasetiaadi <pras.adi.prakasa@gmail.com>
2022-12-12 20:53:24 +0700

    added navlink

--- src/App.js
+++ src/App.js
@@ -1,39 +1,24 @@
-import Directory from "./components/directory/Directory-component";
+import { Routes, Route } from 'react-router-dom';
 
+import Home from './routes/home/Home';
+import Nav from './routes/navigation/Nav';
 
-const App = () => {
-  const categories = [
-    {
-      id: 1,
-      title: 'hats',
-      imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
-    },
-    {
-      id: 2,
-      title: 'jackets',
-      imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
-    },
-    {
-      id: 3,
-      title: 'sneakers',
-      imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
-    },
-    {
-      id: 4,
-      title: 'womens',
-      imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
-    },
-    {
-      id: 5,
-      title: 'mens',
-      imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
-    },
-  ];
-
+const Shop = () => {
   return (
     <div>
-      <Directory categories={categories} />
+      <h1>This page Shop</h1>
     </div>
+  );
+};
+
+const App = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<Nav />}>
+        <Route index element={<Home />} />
+        <Route path="shop" element={<Shop />} />
+      </Route>
+    </Routes>
   );
 };
 
---
+++ src/routes/navigation/Nav.jsx
@@ -0,0 +1,23 @@
+import { Outlet, Link } from 'react-router-dom';
+import { Fragment } from 'react';
+import './nav.styles.scss';
+import {ReactComponent as Logo } from '../../assets/crown.svg';
+
+const Nav = () => {
+  return (
+    <Fragment>
+      <div className="navigation">
+        <Link className="logo-container" to="/">
+          <Logo className="logo" />
+        </Link>
+        <div className="nav-links-container">
+          <Link className="nav-link" to="/shop">
+            Shop
+          </Link>
+        </div>
+      </div>
+      <Outlet />
+    </Fragment>
+  );
+};
+export default Nav;
